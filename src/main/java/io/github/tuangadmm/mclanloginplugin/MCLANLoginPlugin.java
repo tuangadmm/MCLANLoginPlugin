1ec6ddf678a413bf278bacf988b83f5afe7dfa8c
tuangadmm <t2321502792@gmail.com>
2024-03-15 15:11:52 +0700

    init

---
+++ src/main/java/io/github/tuangadmm/mclanloginplugin/MCLANLoginPlugin.java
@@ -0,0 +1,17 @@
+package io.github.tuangadmm.mclanloginplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCLANLoginPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
